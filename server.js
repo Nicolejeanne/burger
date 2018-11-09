a8513506876a44b48cf57edb4d9614e500238a4f
Nicolejeanne <nicolejeanne@users.noreply.github.com>
2018-11-08 21:46:05 -0600

    added styling

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
