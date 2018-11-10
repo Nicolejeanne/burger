b1767440ac4c57230d4aabd8b77e1bc13dbc06b1
Nicolejeanne <nicolejeanne@users.noreply.github.com>
2018-11-10 10:10:22 -0600

    changed file calls

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ USE burgers_db;
 -- Create the table plans.
 CREATE TABLE burgers
 (
-id int NOT NULL AUTO_INCREMENT,
+id INT AUTO_INCREMENT NOT NULL,
 burger_name varchar(255) NOT NULL,
 devoured boolean NOT NULL default false,
+createdAt TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
